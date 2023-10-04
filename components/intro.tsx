73f9fe4c4f2b93f3e7a6d07d9205c486be9b5a02
utkarshkrishna2004 <utkarshmark14@gmail.com>
2023-10-05 00:40:52 +0530

    Added buttons and section-divider

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 // import { useSectionInView } from "@/lib/hooks";
 // import { useActiveSectionContext } from "@/context/active-section-context";
 
@@ -17,7 +18,7 @@ export default function Intro() {
 
    return (
       <section
-        //  ref={ref}
+         //  ref={ref}
          id="home"
          className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
       >
@@ -69,6 +70,61 @@ export default function Intro() {
             enjoy building <span className="italic">sites and apps</span>. My
             focus is <span className="underline">React (Next.js)</span>.
          </motion.h1>
+
+         <motion.div
+            className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+               delay: 0.1,
+            }}
+         >
+            <Link
+               href="#contact"
+               className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+               onClick={() => {
+                  //   setActiveSection("Contact");
+                  //   setTimeOfLastClick(Date.now());
+               }}
+            >
+               Contact Me Here
+               <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+            </Link>
+
+            <a
+               className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 "
+               href="/CV.pdf"
+               download
+            >
+               Download CV{" "}
+               <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+            </a>
+
+            <a
+               className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10 "
+               href="https://linkedin.com"
+               target="_blank"
+            >
+               <BsLinkedin />
+            </a>
+
+            <a
+               className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10 "
+               href="https://github.com"
+               target="_blank"
+            >
+               <FaGithubSquare />
+            </a>
+
+            <a
+               className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10 "
+               href="https://twitter.com"
+               target="_blank"
+            >
+               <FaTwitter />
+            </a>
+            
+         </motion.div>
       </section>
    );
 }
