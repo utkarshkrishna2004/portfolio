5044f0d79679d2c5deb047c935f044f0ac7eccd4
utkarshkrishna2004 <utkarshmark14@gmail.com>
2023-10-06 02:34:17 +0530

    added navbar tracker

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import utkarshImage from "../public/utkarsh.jpeg"
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -9,16 +9,26 @@ import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
 // import { useSectionInView } from "@/lib/hooks";
 // import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
-//    const { ref } = useSectionInView("Home", 0.5);
-//    const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+   const { ref, inView } = useInView({
+      threshold: 0.5,
+   });
+   const { setActiveSection } = useActiveSectionContext();
+
+   useEffect(() => {
+      if (inView) {
+         setActiveSection("Home");
+      }
+   }, [inView, setActiveSection]);
 
    return (
       <section
-         //  ref={ref}
+          ref={ref}
          id="home"
          className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
       >
