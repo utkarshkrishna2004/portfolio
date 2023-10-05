d74b97c349660d48c95a40e1a67f19055be8fe05
utkarshkrishna2004 <utkarshmark14@gmail.com>
2023-10-05 23:15:45 +0530

    Projects section core done.

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -8,6 +8,6 @@ export default function SectionHeading(
     {children}: SectionHeadingProps
 ) {
   return (
-    <h2 className="mb-8 text-3xl font-medium capitalize">{children}</h2>
+    <h2 className="mb-8 text-3xl font-medium text-center capitalize">{children}</h2>
     )
 }
